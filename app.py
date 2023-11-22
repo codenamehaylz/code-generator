212f764c2ff8ea7a1a0035489fc2afbb86aa1a2e
Hayley <86895234+codenamehaylz@users.noreply.github.com>
2023-11-22 22:16:40 +0000

    created template html files

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask()
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template('index.html')
+
+@app.route("/generate/", methods=("GET", "POST"))
+def generate_codes():
+     return render_template('generate.html')
+
+
+if __name__ == '__main__':
+     app.run(debug=True)
