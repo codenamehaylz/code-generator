b2c826ae37691eaf5ad8a51cb6c6116c2fadfb0f
Hayley <86895234+codenamehaylz@users.noreply.github.com>
2023-11-22 22:45:46 +0000

    form accepts a word and displays it on index page

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = '0e1226ee99bc01c03ef7854b56215ba535246ffc00af41c2'
+
+word = ["Word"]
 
 @app.route("/")
 def hello():
-    return render_template('index.html')
+    return render_template('index.html', word=word)
 
 @app.route("/generate/", methods=("GET", "POST"))
 def generate_codes():
-     return render_template('generate.html')
+    if request.method == "POST":
+        entered_word = request.form['word']
+        if not entered_word:
+            flash("You must enter a word or phrase!")
+        else:
+            word.clear()
+            word.append(entered_word)
+            return redirect(url_for('hello'))
+        
+    return render_template('generate.html')
 
 
 if __name__ == '__main__':
