f91aa27916005717b351ce78a74de916394e4cc8
Hayley <86895234+codenamehaylz@users.noreply.github.com>
2023-11-23 21:27:53 +0000

    generated code tuples display on results page

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, url_for, flash, redirect
-from generate_function import generate_code
+from generate_function import generate_codes
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = '0e1226ee99bc01c03ef7854b56215ba535246ffc00af41c2'
@@ -8,6 +8,8 @@ user_input = [{'word': 'Word',
                'length': 2,
                'first_letter_option': ''}]
 
+generated_codes = []
+
 
 @app.route("/")
 def hello():
@@ -15,21 +17,27 @@ def hello():
 
 @app.route("/result/")
 def result():
-    return render_template('result.html', user_input = user_input)
+    return render_template('result.html', 
+                           user_input=user_input,
+                           generated_codes=generated_codes,
+                           len=len(generated_codes))
 
 @app.route("/generate/", methods=("GET", "POST"))
-def generate_codes():
+def generate():
     if request.method == "POST":
         word = request.form.get('word')
-        length = request.form.get('characters')
+        length = int(request.form.get('characters'))
         option = request.form.get('first_char')
-
 
         if not word:
             flash("You must enter a word or phrase!")
         else:
             user_input.clear()
             user_input.append({'word': word, 'length': length, 'first_letter_option': option})
+            codes = generate_codes(word, length, option)
+            generated_codes.clear()
+            for code in codes:
+                generated_codes.append(code)
             return redirect(url_for('result'))
         
     return render_template('generate.html')
--- generate_function.py
+++ generate_function.py
@@ -4,13 +4,13 @@ import itertools
 # code_length = int(input("How many characters should the generated code have?: "))
 # first_letter_option = input("Should the generated code start with the first letter of your word/phrase?: ")
 
-def generate_code(word, length, first_letter):
+def generate_codes(word, length, first_letter):
     generated = itertools.permutations(word, length)
 
     results = []
 
     for i in generated:
-        if first_letter:
+        if first_letter == 'on':
             if i not in results and i[0] == word[0]:
                 results.append(i)
         else:
